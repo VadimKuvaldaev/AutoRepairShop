a74e958612225a4729288597c95a413f1de69437
VadimKuvaldaev <vadimkuvaldaev@gmail.com>
2026-02-19 09:30:26 +0300

    Добавлена таблица ModelsCars и начата работа со страницами

--- AutoRepairShop/Pages/Index.cshtml.cs
+++ AutoRepairShop/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using AutoRepairShop.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -6,15 +7,17 @@ namespace AutoRepairShop.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public void OnGet()
         {
-
+            var Cars = _context.Cars.ToList();
         }
     }
 }
