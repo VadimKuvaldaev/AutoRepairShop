d2e8fd54c54f0acba84a399b973982150f050393
VadimKuvaldaev <vadimkuvaldaev@gmail.com>
2026-03-05 09:27:48 +0300

    Доделать страницы, решить ошибки

---
+++ AutoRepairShop/Pages/Clients/Index.cshtml.cs
@@ -0,0 +1,21 @@
+using AutoRepairShop.Data;
+using AutoRepairShop.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutoRepairShop.Pages.Clients
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        public IndexModel(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+        public List<Client> Clients { get; set; }
+        public void OnGet()
+        {
+            Clients = _context.Clients.ToList();
+        }
+    }
+}
