aba36b93cf9595bbaf954d513ee1a98e287a6202
VadimKuvaldaev <vadimkuvaldaev@gmail.com>
2026-04-11 22:06:09 +0300

    Страницы переделаны в bootstrap, добавлена темная тема. Решить проблемы с владельцами авто

--- AutoRepairShop/Pages/Cars/Create.cshtml.cs
+++ AutoRepairShop/Pages/Cars/Create.cshtml.cs
@@ -2,6 +2,7 @@ using AutoRepairShop.Data;
 using AutoRepairShop.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AutoRepairShop.Pages.Cars
 {
@@ -17,17 +18,37 @@ namespace AutoRepairShop.Pages.Cars
         [BindProperty]
         public Car Car { get; set; }
 
-        public void OnGet() { }
+        public SelectList ClientList { get; set; }
+
+        public void OnGet()
+        {
+            PopulateClientsList();
+        }
 
         public IActionResult OnPost()
         {
+            ModelState.Remove("Car.Client");
+            ModelState.Remove("Car.Brand");
+
             if (!ModelState.IsValid)
+            {         
+                PopulateClientsList();
                 return Page();
+            }
 
             _context.Cars.Add(Car);
             _context.SaveChanges();
+            return RedirectToPage("./Index");
+        }
 
-            return RedirectToPage("Index");
+        private void PopulateClientsList()
+        {
+            var clients = _context.Clients
+                .OrderBy(c => c.FullName)
+                .Select(c => new { c.Id, c.FullName })
+                .ToList();
+
+            ClientList = new SelectList(clients, "Id", "FullName");
         }
     }
 }
--- AutoRepairShop/Pages/Cars/Details.cshtml.cs
+++ AutoRepairShop/Pages/Cars/Details.cshtml.cs
@@ -2,6 +2,7 @@ using AutoRepairShop.Data;
 using AutoRepairShop.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutoRepairShop.Pages.Cars
 {
@@ -16,12 +17,22 @@ namespace AutoRepairShop.Pages.Cars
 
         public Car Car { get; set; }
 
-        public IActionResult OnGet(int id)
+        public IActionResult OnGet(int? id)
         {
-            Car = _context.Cars.FirstOrDefault(b => b.Id == id);
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Car = _context.Cars
+                .Include(c => c.Brand)
+                .Include(c => c.Client)
+                .FirstOrDefault(m => m.Id == id);
 
             if (Car == null)
+            {
                 return NotFound();
+            }
 
             return Page();
         }
