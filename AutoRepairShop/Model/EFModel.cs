de06521c5281398516c7d2e9f138ec41656cc279
VadimKuvaldaev <vadimkuvaldaev@gmail.com>
2026-04-23 08:52:50 +0300

    Исправлена валидация

--- AutoRepairShop/Model/EFModel.cs
+++ AutoRepairShop/Model/EFModel.cs
@@ -5,8 +5,6 @@ namespace AutoRepairShop.Model
     public class EFModel
     {
         public int Id { get; set; }
-
-        [Required(ErrorMessage = "Поле 'Имя/Наименование обязательно для заполнения'")]
         public string Name { get; set; } = "";
     }
 }
